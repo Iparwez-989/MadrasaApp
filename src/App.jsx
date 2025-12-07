9052aebf7ddf4cb8343d21f064ff1676e2566442
Irfan Parwez <iparwez351412@gmail.com>
2025-12-07 11:26:23 +0530

    Nav

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import BottomNav from './components/BottomNav'
 
 const App = () => {
   return (
-    <div className='flex flex-col h-screen justify-between no-scroll'>
+    <div className='flex flex-col h-screen justify-between'>
       <div>
       <Header />
       <Home />
--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 
 const BottomNav = () => {
   return (
-    <div className='bg-slate-200  h-16 flex justify-around items-center text-gray-600 p-3'>
-      <div>
-        <HouseSimpleIcon size={30} />
+    <div className='fixed bottom-0 left-0 w-full text-sm font-semibold bg-slate-200  h-16 flex justify-around items-center text-gray-600 p-4'>
+      <div className='flex flex-col items-center '>
+        <HouseSimpleIcon size={28} />
         <p>Home</p>
       </div>
-      <div>
-        <BookOpenTextIcon size={30} />
-        <p>Quran</p>
+      <div className='flex flex-col items-center '>
+        <BookOpenTextIcon size={28} />
+        <p className=''>Quran</p>
         </div>
-        <div className='bg-[#6d2dd3] rounded-full p-3 text-black'>
-            <ListHeartIcon size={30} />
+        <div className='bg-[#6d2dd3] rounded-full p-3 text-black '>
+            <ListHeartIcon size={28} />
         </div>
-      <div>
-        <MosqueIcon size={30} />
+      <div className='flex flex-col items-center '>
+        <MosqueIcon size={28} />
         <p>Maktab</p>
         </div>
-      <div>
-        <ListHeartIcon size={30} />
+      <div className='flex flex-col items-center '>
+        <ListHeartIcon size={28} />
         <p>Dua</p>
         </div>
       
